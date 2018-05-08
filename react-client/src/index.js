96676c1e5f5f68846bb47c854c050961ac235270
darthchudi <chukwudioranu@ymail.com>
2018-05-08 16:48:30 +0100

    chore: create production react build

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import App from './App';
 import SignUp from './pages/SignUp';
@@ -11,12 +11,12 @@ import './styles/bootstrap/dist/css/bootstrap.min.css';
 import './styles/yarns.css';
 
 ReactDOM.render((
-	<HashRouter>
+	<BrowserRouter>
 		<div>
 			<PrivateRoute exact path="/" component={App}/>
 			<Route path="/signup" component={SignUp}/>		
 			<Route path="/login" component={Login}/>	
 		</div>
-	</HashRouter>
+	</BrowserRouter>
 	), document.getElementById('root'));
 registerServiceWorker();
